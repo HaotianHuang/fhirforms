d5a07fa8ae582c8e36330676ddc59719f65365f6
Haotian Huang <haotianhuang@gmail.com>
2023-09-09 14:31:12 -0700

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        form_url = request.form['form_url']
+        return redirect(url_for('form', form_url=form_url))
+    return render_template('home.html')
+
+@app.route('/form')
+def form():
+    form_url = request.args.get('form_url')
+    return render_template('form.html', form_url=form_url)
+
+@app.route('/results', methods=['GET'])
+def get_results():
+    form_url = request.args.get('form_url')
+    return render_template('results.html', form_url=form_url)
+
+
+if __name__ == '__main__':
+    app.run()
